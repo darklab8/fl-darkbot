1872317299605a4545e65be04b99d59210f17132
dd84ai <dd84ai@gmail.com>
2021-05-04 22:33:20 +0700

    feat: started

---
+++ README.md
@@ -0,0 +1,12 @@
+# Description
+
+* bot for [Discovery Freelancer community](https://discoverygc.com/forums/index.php)
+
+# Link
+* [bot invite link](https://discord.com/oauth2/authorize?client_id=838460303581904949&permissions=8&scope=bot)
+
+# Planned features
+
+* Player online in certain in-game systems
+* Showing info about player bases
+* Showing available for sale or buying ores/devices and other resources
---
+++ app.py
@@ -0,0 +1,94 @@
+import discord
+import os
+import json
+from types import SimpleNamespace
+from dotenv import load_dotenv
+
+from discord.ext import commands
+
+load_dotenv()
+settings = SimpleNamespace()
+for item, value in os.environ.items():
+    # print('{}: {}'.format(item, value))
+    setattr(settings, item, value)
+
+# with open('secret.json') as file_:
+#     settings = SimpleNamespace(**(json.loads(file_.read())))
+
+SECRET_KEY = settings.secret_key
+command_start = '$d '
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+        # if message.content.startswith(command_start):
+        #     if 'hello' in message.content:
+        #         await message.channel.send('hi there!')
+        #     if 'help' in message.content:
+        #         await message.channel.send(
+        #             'help command list will be implemented later!')
+        # and "darkwind" in message.author.name.lower():
+        #
+
+
+APIDATA = SimpleNamespace()
+
+from discord.ext import tasks, commands
+import requests
+import random
+
+
+class MyCog(commands.Cog):
+    def __init__(self, bot):
+        self.index = 0
+        self.bot = bot
+        self.printer.start()
+
+    def cog_unload(self):
+        self.printer.cancel()
+        print('unloading')
+
+    @tasks.loop(seconds=5.0)
+    async def printer(self):
+        #print(self.index)
+        APIDATA.players = requests.get(settings.player_request_url).json()
+        APIDATA.bases = requests.get(settings.base_request_url).json()
+        self.index += 1
+
+    @printer.before_loop
+    async def before_printer(self):
+        print('waiting...')
+        await self.bot.wait_until_ready()
+
+
+bot = MyClient()
+
+cog = MyCog(bot)
+
+bot = commands.Bot(command_prefix='$')
+
+
+@bot.command(name='fun')
+async def nine_nine(ctx):
+    "says random message"
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        ('Cool. Cool cool cool cool cool cool cool, '
+         'no doubt no doubt no doubt no doubt.'),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+
+# @bot.command(name='help')
+# async def helping(ctx):
+#     await ctx.send('one day, you will find help here')
+
+bot.run(SECRET_KEY)
