39c96c09713c42dd51d35aa62099bd1a86ef4b1d
dd84ai <dd84ai@gmail.com>
2022-07-14 02:54:22 +0200

    refactor: test factory

--- src/scrappy/players/tests.py
+++ src/scrappy/players/tests.py
@@ -1,5 +1,21 @@
 from .repository import PlayerRepository
+from faker import Faker
 from .schemas import PlayerSchema
+
+fake = Faker()
+
+class PlayerTestFactory:
+    repo_model = PlayerRepository
+
+    def __new__(
+        cls, 
+        db,
+        **kwargs: dict
+    ) -> PlayerSchema:
+        repo = cls.repo_model(db)
+        return repo.create_one(
+            description=kwargs.get("description", fake.name())
+        )
 
 def test_check_db(db):
     player_repo = PlayerRepository(db)
@@ -12,3 +28,14 @@ def test_check_db(db):
 
     assert player.id == 1
     assert player.description == "abc"
+
+
+def test_check_test_factory(db):
+
+    player = PlayerTestFactory(db)
+    assert player.id == 1
+    assert isinstance(player.description, str)
+
+
+def test_check_endpoint_to_get_players(db):
+    pass
