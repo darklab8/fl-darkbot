73679b77b74e9bfd0236e8f3546d3d2351947bde
dd84ai <dd84ai@gmail.com>
2022-07-29 15:28:31 +0200

    refactor: separate command

--- make.py
+++ make.py
@@ -30,7 +30,6 @@ class Services(EnumWithValues):
 def shell(cmd):
     exit(os.system(cmd))
 
-random_string = secrets.token_hex(16)
 parser = argparse.ArgumentParser()
 parser.add_argument('--job_id', type=str, default=secrets.token_hex(4),
     help="optional parameter random by default, ensures to run docker-compose with random -p parameter for no conflicts in parallel runs",
@@ -39,25 +38,33 @@ parser.add_argument('service', type=str, choices=Services.get_keys())
 parser.add_argument('action', type=str, choices=Actions.get_keys())
 args = parser.parse_args()
 
-def run_inside_container(command):
+def run_inside_container(service, command):
     return_code = os.system(
-        f"docker-compose -f docker-compose.{args.service}.yml -p {args.job_id} build && "
-        f"docker-compose -f docker-compose.{args.service}.yml  -p {args.job_id}"
+        f"docker-compose -f docker-compose.{service}.yml -p {args.job_id} build && "
+        f"docker-compose -f docker-compose.{service}.yml  -p {args.job_id}"
         f" {command}"
     )
     os.system(
-        f"docker-compose -f docker-compose.{args.service}.yml  -p {args.job_id} down"
+        f"docker-compose -f docker-compose.{service}.yml  -p {args.job_id} down"
     )
     print(return_code)
     if return_code != 0:
         raise Exception(f"non zero returned code={return_code}")
 
-match args.action:
-    case Actions.test:
-        run_inside_container(command="run --rm service_base pytest")
-    case Actions.shell:
-        run_inside_container(command="run --rm service_base /bin/bash")
-    case Actions.run:
-        run_inside_container(command="up")
-    case Actions.lint:
-        run_inside_container(command="run --rm service_base black --check .")
+class CommonCommands:
+    test = "run --rm service_base pytest"
+    shell = "run --rm service_base /bin/bash"
+    run = "up"
+    lint = "run --rm service_base black --check ."
+
+match (args.service, args.action):
+    case (Services.scrappy, Actions.test):
+        run_inside_container(service=args.service, command=CommonCommands.test)
+    case (Services.scrappy, Actions.shell):
+        run_inside_container(service=args.service, command=CommonCommands.shell)
+    case (Services.scrappy, Actions.run):
+        run_inside_container(service=args.service, command=CommonCommands.run)
+    case (Services.scrappy, Actions.lint):
+        run_inside_container(service=args.service, command=CommonCommands.lint)
+    case _:
+        raise Exception("Not registered command for this service")
