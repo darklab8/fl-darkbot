74f3f2b4fdeda3d05631c483b0a62a0ffb8bb699
dd84ai <dd84ai@gmail.com>
2022-08-07 07:25:17 +0200

    fix: CI

--- src/scrappy/bases/tests.py
+++ src/scrappy/bases/tests.py
@@ -1,6 +1,8 @@
 from .subtasks import SubTaskGetBaseData
+import pytest
 
 
+@pytest.mark.integration
 def test_request_base_url():
     data = SubTaskGetBaseData()
     from pprint import pprint as print
