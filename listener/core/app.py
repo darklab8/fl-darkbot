4295041af1a32f42f58c038fd82854745b0a6a9d
dd84ai <dd84ai@gmail.com>
2022-08-13 05:05:52 +0200

    feat: permission checking

--- listener/core/app.py
+++ listener/core/app.py
@@ -1,5 +1,6 @@
 import discord
 from .connector import run_command_async
+from listener.permissions import PermissionChecker
 
 
 class MyClient(discord.Client):
@@ -14,16 +15,31 @@ class MyClient(discord.Client):
         content: str = message.content
         print(f"content={content}")
 
+        permissions = PermissionChecker(message)
+        print(f"permissions.all={permissions.is_having_any_permission}")
+        print(f"permissions.predicate_controller={permissions.predicate_controller}")
+        print(f"permissions.predicate_guild_owner={permissions.predicate_guild_owner}")
+        print(f"permissions.predicate_manage={permissions.predicate_manage}")
+        print(f"permissions.predicate_owner={permissions.predicate_owner}")
+
+        if not permissions.is_having_any_permission:
+            await message.channel.send(
+                "not authorized user. required to have role 'bot_controller', or right to `manage channels`, or being guild owner"
+            )
+            return
+
         prefix = ".bot"
-        if content.startswith(prefix):
-            content = content[len(prefix) :]
+        if not content.startswith(prefix):
+            return
 
-            args = [arg for arg in content.split(" ") if arg != ""]
-            print(f"args={args}")
+        content = content[len(prefix) :]
 
-            result = await run_command_async(*(["python3", "-m" "consoler"] + args))
+        args = [arg for arg in content.split(" ") if arg != ""]
+        print(f"args={args}")
 
-            # print(f"answer={result}")
+        result = await run_command_async(*(["python3", "-m" "consoler"] + args))
 
-            # await message.channel.send("pong")
-            await message.channel.send(result)
+        # print(f"answer={result}")
+
+        # await message.channel.send("pong")
+        await message.channel.send(result)
