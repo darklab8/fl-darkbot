6a261a8b7087463197328d158f578291b03916a2
dd84ai <dd84ai@gmail.com>
2023-01-08 20:49:22 +0100

    feat: defining plans for discorder package

---
+++ discorder/main.go
@@ -0,0 +1,8 @@
+/*
+TODO implement interface
+- to capture message history from channel (internal)
+- to create or replace message in channel (public?)
+- delete message from channel (public?)
+*/
+
+package discorder
