d43874763a40a96a0e3494e3496d7cf50743d17a
dd84ai <dd84ai@gmail.com>
2023-01-09 22:03:52 +0100

    feat: migrating command

---
+++ cmd/migrate.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"darkbot/configurator"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// migrateCmd represents the migrate command
+var migrateCmd = &cobra.Command{
+	Use:   "migrate",
+	Short: "Migrate db",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("migrate called")
+		configurator.GetConnection()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(migrateCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// migrateCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// migrateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
