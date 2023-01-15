a48778993130140c23cbe13ab2404484ab6e56ba
dd84ai <dd84ai@gmail.com>
2023-01-15 17:06:53 +0100

    test: fix them to new prefix

--- consoler/consoler_test.go
+++ consoler/consoler_test.go
@@ -8,16 +8,16 @@ import (
 )
 
 func TestGettingOutput(t *testing.T) {
-	assert.Contains(t, Consoler{}.New("ping").Execute(helper.ChannelInfo{ChannelID: "123"}).String(), "Pong!")
+	assert.Contains(t, Consoler{}.New(". ping").Execute(helper.ChannelInfo{ChannelID: "123"}).String(), "Pong!")
 }
 
 func TestGrabStdout(t *testing.T) {
-	c := Consoler{}.New("ping --help")
+	c := Consoler{}.New(". ping --help")
 	result := c.Execute(helper.ChannelInfo{ChannelID: "123"}).String()
 
 	assert.Contains(t, result, "\nFlags:\n  -h, --help   ")
 }
 
 func TestAddBaseTag(t *testing.T) {
-	assert.Contains(t, Consoler{}.New(`base add "bla bla" sdf`).Execute(helper.ChannelInfo{ChannelID: "123"}).String(), "OK tags are added")
+	assert.Contains(t, Consoler{}.New(`. base add "bla bla" sdf`).Execute(helper.ChannelInfo{ChannelID: "123"}).String(), "OK tags are added")
 }
