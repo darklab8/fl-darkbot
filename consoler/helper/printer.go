9254f70fd59e5889b3e2d6a19353826682092aea
dd84ai <dd84ai@gmail.com>
2023-01-15 16:40:40 +0100

    fix: bug with incorrect output

--- consoler/helper/printer.go
+++ consoler/helper/printer.go
@@ -1,11 +1,19 @@
 package helper
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 type Printer struct {
 	Cmd *cobra.Command
 }
 
 func (p Printer) Print(msg string) {
-	p.Cmd.OutOrStdout().Write([]byte("OK tags are added"))
+	p.Cmd.OutOrStdout().Write([]byte(msg))
+}
+
+func (p Printer) Println(msg string) {
+	p.Cmd.OutOrStdout().Write([]byte(fmt.Sprintf("%s\n", msg)))
 }
