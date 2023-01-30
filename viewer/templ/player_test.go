af76e20bc4d999555de695d95718f48637da739e
dd84ai <dd84ai@gmail.com>
2023-01-30 01:02:51 +0100

    test: to render player view

--- viewer/templ/player_test.go
+++ viewer/templ/player_test.go
@@ -3,26 +3,31 @@ package templ
 import (
 	"darkbot/configurator"
 	"darkbot/dtypes"
+	"darkbot/scrappy"
+	"darkbot/scrappy/player"
+	"darkbot/scrappy/shared/records"
+	"fmt"
 	"testing"
 )
 
 func TestPlayerViewer(t *testing.T) {
 	configurator.FixtureMigrator(func(dbpath dtypes.Dbpath) {
 		channelID, _ := configurator.FixtureChannel(dbpath)
-		_ = channelID
+
+		playerCfg := configurator.ConfiguratorBase{Configurator: configurator.NewConfigurator(dbpath)}
+		playerCfg.TagsAdd(channelID, []string{"Station"}...)
+
+		players := player.PlayerStorage{}
+		scrappy.Storage = &scrappy.ScrappyStorage{PlayerStorage: &players}
+		record := records.StampedObjects[player.Player]{}.New()
+		record.Add("Station1", player.Player{Name: "Abc", System: "New York"})
+		record.Add("Station2", player.Player{Name: "Qwe", System: "Hamburg"})
+		players.Add(record)
+
+		playerView := NewTemplatePlayers(channelID, dbpath)
+		playerView.Render()
+		fmt.Println(playerView.friends.Content)
+		fmt.Println(playerView.enemies.Content)
+		fmt.Println(playerView.all.Content)
 	})
-
-	// cg := configurator.ConfiguratorBase{Configurator: configurator.NewConfigurator()}
-	// cg.TagsAdd(channelID, []string{"Station"}...)
-
-	// bases := base.BaseStorage{}
-	// scrappy.Storage = &scrappy.ScrappyStorage{BaseStorage: &bases}
-	// record := records.StampedObjects[base.Base]{}.New()
-	// record.Add("Station1", base.Base{Name: "Station1", Affiliation: "Abc", Health: 100})
-	// record.Add("Station2", base.Base{Name: "Station2", Affiliation: "Qwe", Health: 100})
-	// bases.Add(record)
-
-	// base := NewTemplateBase(channelID)
-	// base.Render()
-	// fmt.Println(base.main.Content)
 }
