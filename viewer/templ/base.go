e72312ff0d804935e1284a9ef90e2da4d1eef648
dd84ai <dd84ai@gmail.com>
2023-02-20 00:52:13 +0100

    feat: base alert, health lower than X

--- viewer/templ/base.go
+++ viewer/templ/base.go
@@ -6,6 +6,7 @@ import (
 	"darkbot/utils"
 	"darkbot/viewer/apis"
 	_ "embed"
+	"fmt"
 	"sort"
 	"strings"
 	"text/template"
@@ -23,14 +24,20 @@ func init() {
 // Base
 
 type TemplateBase struct {
-	main TemplateShared
-	API  apis.API
+	main                    TemplateShared
+	AlertHealthLowerThan    TemplateShared
+	AlertHealthIsDecreasing TemplateShared
+	AlertBaseUnderAttack    TemplateShared
+	API                     apis.API
 }
 
 func NewTemplateBase(channelID string, dbpath dtypes.Dbpath) TemplateBase {
 	base := TemplateBase{}
 	base.API = apis.NewAPI(channelID, dbpath)
 	base.main.Header = "#darkbot-base-view"
+	base.AlertHealthLowerThan.Header = "#darkbot-base-alert-health-lower-than"
+	base.AlertHealthIsDecreasing.Header = "#darkbot-base-health-is-decreasing"
+	base.AlertBaseUnderAttack.Header = "#darkbot-base-base-under-attack"
 	return base
 }
 
@@ -51,9 +58,15 @@ func BaseContainsTag(bas base.Base, tags []string) bool {
 }
 
 func (b *TemplateBase) Setup(channelID string) {
+	b.API.ChannelID = channelID
 	b.main.MessageID = ""
 	b.main.Content = ""
-	b.API.ChannelID = channelID
+	b.AlertHealthLowerThan.MessageID = ""
+	b.AlertHealthLowerThan.Content = ""
+	b.AlertHealthIsDecreasing.MessageID = ""
+	b.AlertHealthIsDecreasing.Content = ""
+	b.AlertBaseUnderAttack.MessageID = ""
+	b.AlertBaseUnderAttack.Content = ""
 }
 
 func (b *TemplateBase) Render() {
@@ -86,15 +99,37 @@ func (b *TemplateBase) Render() {
 	}
 
 	b.main.Content = utils.TmpRender(baseTemplate, input)
+
+	// Alerts
+	if healthThreshold, _ := b.API.Alerts.BaseHealthLowerThan.Status(b.API.ChannelID); healthThreshold != nil {
+		for _, base := range record.List {
+			if int(base.Health) < *healthThreshold {
+				b.AlertHealthLowerThan.Content = RenderAlertTemplate(b.AlertHealthLowerThan.Header, b.API.ChannelID, fmt.Sprintf("Base %s has health %d lower than threshold %d", base.Name, int(base.Health), *healthThreshold), b.API)
+			}
+		}
+	}
+
 }
 
 func (t *TemplateBase) Send() {
 	t.main.Send(t.API)
+	t.AlertHealthLowerThan.Send(t.API)
+	t.AlertHealthIsDecreasing.Send(t.API)
+	t.AlertBaseUnderAttack.Send(t.API)
 }
 
 func (t *TemplateBase) MatchMessageID(messageID string) bool {
 
 	if messageID == t.main.MessageID {
+		return true
+	}
+	if messageID == t.AlertHealthLowerThan.MessageID {
+		return true
+	}
+	if messageID == t.AlertHealthIsDecreasing.MessageID {
+		return true
+	}
+	if messageID == t.AlertBaseUnderAttack.MessageID {
 		return true
 	}
 	return false
@@ -103,5 +138,14 @@ func (t *TemplateBase) MatchMessageID(messageID string) bool {
 func (t *TemplateBase) DiscoverMessageID(content string, msgID string) {
 	if strings.Contains(content, t.main.Header) {
 		t.main.MessageID = msgID
+	}
+	if strings.Contains(content, t.AlertHealthLowerThan.Header) {
+		t.AlertHealthLowerThan.MessageID = msgID
+	}
+	if strings.Contains(content, t.AlertHealthIsDecreasing.Header) {
+		t.AlertHealthIsDecreasing.MessageID = msgID
+	}
+	if strings.Contains(content, t.AlertBaseUnderAttack.Header) {
+		t.AlertBaseUnderAttack.MessageID = msgID
 	}
 }
