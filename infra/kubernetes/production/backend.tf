8ca1515f72f2d7cc18da5268f013de2253696799
dd84ai <dd84ai@gmail.com>
2023-11-05 15:58:29 +0100

    refactor: deprecate ansible scripts

--- infra/kubernetes/production/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "darklab-eu-west-1-global-terraform-state"
-    dynamodb_table = "darklab-eu-west-1-global-terraform-state"
-    encrypt        = true
-    key            = "darkbot.kube.production.tfstate"
-    region         = "eu-west-1"
-  }
-}
