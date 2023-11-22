d199643dddcdc6b4657aa53e40302f63f5b93831
dd84ai <dd84ai@gmail.com>
2023-11-22 22:19:37 +0100

    fix: corrupted channels better commands

--- app/management/channels.go
+++ app/management/channels.go
@@ -5,6 +5,7 @@ package management
 
 import (
 	"darkbot/app/configurator"
+	"darkbot/app/discorder"
 	"darkbot/app/settings"
 	"darkbot/app/settings/logus"
 	"darkbot/app/settings/types"
@@ -20,20 +21,44 @@ var channelDeleteCMD = &cobra.Command{
 		logus.Info("Cmd is called with args=", logus.Args(args))
 		channels := configurator.NewConfiguratorChannel(configurator.NewConfigurator(settings.Dbpath))
 
+		fmt.Println("trying to delete ", len(args), args)
+		var errors []error
 		for _, channeID := range args {
-			channels.Remove(types.DiscordChannelID(channeID))
+			errors = append(errors, channels.Remove(types.DiscordChannelID(channeID)))
 		}
+
+		fmt.Println("error count", len(errors), " | ", errors)
 	},
 }
 var channelListCMD = &cobra.Command{
 	Use:   "channel_list",
 	Short: "List channelIDs",
 	Run: func(cmd *cobra.Command, args []string) {
+		dg := discorder.NewClient()
 		logus.Info("Cmd is called with args=", logus.Args(args))
 		channels := configurator.NewConfiguratorChannel(configurator.NewConfigurator(settings.Dbpath))
 
 		channelIDs, _ := channels.List()
-		fmt.Println("channelIDs=", channelIDs)
+		fmt.Println("all channelIDs", len(channelIDs), " | ", channelIDs)
+
+		var accessable_channels []types.DiscordChannelID
+		var error_channels []types.DiscordChannelID
+		var error_reason []error
+
+		for _, channel := range channelIDs {
+			_, err := dg.GetLatestMessages("838802002582175756")
+
+			if err == nil {
+				accessable_channels = append(accessable_channels, channel)
+			} else {
+				error_channels = append(error_channels, channel)
+				error_reason = append(error_reason, err)
+			}
+		}
+
+		fmt.Println("accessable_channels", len(accessable_channels), " | ", accessable_channels)
+		fmt.Println("error_channels", len(error_channels), " | ", error_channels)
+		fmt.Println("error_reason", len(error_reason), " | ", error_reason)
 	},
 }
 
--- infra/tf/staging/main.tf
+++ infra/tf/staging/main.tf
@@ -27,7 +27,7 @@ module "darkbot" {
   configurator_dbname = "staging"
   consoler_prefix     = ","
   secrets             = local.secrets
-  tag_version         = "v1.2.0"
+  tag_version         = "v1.2.2"
   debug               = false
 }
 
