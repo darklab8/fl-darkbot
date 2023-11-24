753fb8d08b5de7bf2507429a9f7464f69c0e401a
dd84ai <dd84ai@gmail.com>
2023-11-24 03:43:07 +0100

    fix: timeout to higher one

--- infra/tf/staging/main.tf
+++ infra/tf/staging/main.tf
@@ -27,7 +27,7 @@ module "darkbot" {
   configurator_dbname = "staging"
   consoler_prefix     = ","
   secrets             = local.secrets
-  tag_version         = "v1.3.0"
+  tag_version         = "v1.3.1"
   debug               = false
 }
 
