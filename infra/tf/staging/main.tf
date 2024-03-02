9092d88b66215f36283a36735ec8516ca1a59384
dd84ai <dd84ai@gmail.com>
2024-03-02 22:33:12 +0100

    refactor: take down staging

--- infra/tf/staging/main.tf
+++ infra/tf/staging/main.tf
@@ -1,20 +1,20 @@
-module "stack" {
-  source       = "../modules/hetzner_server"
-  environment  = "staging"
-  server_power = "cpx21"
-  backups      = false
-}
+# module "stack" {
+#   source       = "../modules/hetzner_server"
+#   environment  = "staging"
+#   server_power = "cpx21"
+#   backups      = false
+# }
 
-output "cluster_ip" {
-  value = module.stack.cluster_ip
-}
+# output "cluster_ip" {
+#   value = module.stack.cluster_ip
+# }
 
-provider "docker" {
-  host     = "ssh://root@${module.stack.cluster_ip}:22"
-  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null", "-i", "~/.ssh/id_rsa.darklab"]
-}
+# provider "docker" {
+#   host     = "ssh://root@${module.stack.cluster_ip}:22"
+#   ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null", "-i", "~/.ssh/id_rsa.darklab"]
+# }
 
-data "aws_ssm_parameter" "github.com/darklab8/fl-darkbot" {
+data "aws_ssm_parameter" "darkbot" {
   name = "/terraform/hetzner/darkbot/staging"
 }
 
@@ -22,30 +22,11 @@ locals {
   secrets = nonsensitive(jsondecode(data.aws_ssm_parameter.darkbot.value))
 }
 
-module "github.com/darklab8/fl-darkbot" {
-  source              = "../modules/darkbot"
-  configurator_dbname = "staging"
-  consoler_prefix     = ","
-  secrets             = local.secrets
-  tag_version         = "v1.5.0"
-  debug               = false
-}
-
-# # Create a docker image resource
-# # -> docker pull nginx:latest
-# resource "docker_image" "nginx" {
-#   name         = "nginx:latest"
-#   keep_locally = true
-# }
-
-# # Create a docker container resource
-# # -> same as 'docker run --name nginx -p8080:80 -d nginx:latest'
-# resource "docker_container" "nginx" {
-#   name    = "nginx"
-#   image   = docker_image.nginx.image_id
-
-#   ports {
-#     external = 8080
-#     internal = 80
-#   }
+# module "darkbot" {
+#   source              = "../modules/darkbot"
+#   configurator_dbname = "staging"
+#   consoler_prefix     = ","
+#   secrets             = local.secrets
+#   tag_version         = "v1.5.0"
+#   debug               = false
 # }
