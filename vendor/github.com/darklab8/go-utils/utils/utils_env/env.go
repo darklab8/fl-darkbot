17e53b7d6b4a21bc978d0df8897ddd09882fabb8
dd84ai <dd84ai@gmail.com>
2024-06-08 08:23:04 +0200

    refactor: go mod vendor

--- vendor/github.com/darklab8/go-utils/utils/utils_env/env.go
+++ vendor/github.com/darklab8/go-utils/utils/utils_env/env.go
@@ -1,6 +1,12 @@
 package utils_env
 
-import "os"
+import (
+	"os"
+	"strconv"
+
+	"github.com/darklab8/go-typelog/typelog"
+	"github.com/darklab8/go-utils/utils/utils_logus"
+)
 
 type EnvConfig struct{}
 
@@ -20,6 +26,22 @@ func (e EnvConfig) GetEnvBool(key string) bool {
 	return os.Getenv(key) == "true"
 }
 
+func (e EnvConfig) GetEnvBoolWithDefault(key string, default_ bool) bool {
+	if value, ok := os.LookupEnv(key); ok {
+		return value == "true"
+	}
+	return default_
+}
+
 func (e EnvConfig) GetEnv(key string) string {
 	return os.Getenv(key)
+}
+
+func (e EnvConfig) GetIntWithDefault(key string, default_ int) int {
+	if value, ok := os.LookupEnv(key); ok {
+		int_value, err := strconv.Atoi(value)
+		utils_logus.Log.CheckPanic(err, "expected to be int", typelog.String("key", key))
+		return int_value
+	}
+	return default_
 }
