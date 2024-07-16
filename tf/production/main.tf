02ca6ba857a231f1c287aa6d23ecd7fe55b95e56
dd84ai <dd84ai@gmail.com>
2024-07-17 00:36:26 +0200

    ops: deploy 1.5.3 version

--- tf/production/main.tf
+++ tf/production/main.tf
@@ -25,7 +25,7 @@ module "darkbot" {
   configurator_dbname = "production"
   consoler_prefix     = "."
   secrets             = local.secrets
-  tag_version         = "v1.5.1-arm"
+  tag_version         = "v1.5.3"
   mode                = "docker"
   environment         = "production"
 }
