6c9567e272d4b3e10583b4e72e3c3850633dc1b8
dd84ai <dd84ai@gmail.com>
2026-02-17 22:25:52 +0100

    test: add rememberance why you need player register observer

--- app/discorder/main_test.go
+++ app/discorder/main_test.go
@@ -3,6 +3,7 @@ package discorder
 import (
 	"testing"
 
+	"github.com/darklab8/fl-darkbot/app/scrappy/player"
 	"github.com/darklab8/fl-darkbot/app/settings/logus"
 )
 
@@ -13,4 +14,21 @@ func TestLogging(t *testing.T) {
 
 	logus.Log.Warn("send long msg", logus.MsgContent(long_test))
 	logus.Log.Warn("send shot msg", logus.MsgContent(short_test))
+}
+
+func TestReceivePlayersNumbersUpdate(t *testing.T) {
+	// unit test by static typing check it is present :)
+
+	if false {
+		return
+	}
+
+	player_api := player.FixturePlayerAPIMock()
+	player_storage := player.NewPlayerStorage(player_api)
+	dg := NewClient()
+	player_storage.RegisterObserve(dg) // updates number of players in Bot description
+
+	player_storage.Update() // has at the end call of observers
+	player_storage.UpdateObservers()
+
 }
